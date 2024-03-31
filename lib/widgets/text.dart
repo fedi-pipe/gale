2c2a6688c63cc6c7a2444304d97b919c73df2168
malkoG <rijgndqw012@gmail.com>
2024-03-31 18:01:07 +0900

    Add support fontweight predicates for GaleText component

--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -3,7 +3,42 @@ import 'package:gale/core/base.dart';
 import 'package:gale/core/color.dart';
 import 'package:gale/core/widget_base.dart';
 
-class GaleText extends StatelessWidget with hasTextColor implements GaleWidget {
+class GaleFont {
+  static get thin => GaleFontWeight(FontWeight.w100);
+  static get extraLight => GaleFontWeight(FontWeight.w200);
+  static get light => GaleFontWeight(FontWeight.w300);
+  static get normal => GaleFontWeight(FontWeight.w400);
+  static get medium => GaleFontWeight(FontWeight.w500);
+  static get semibold => GaleFontWeight(FontWeight.w600);
+  static get bold => GaleFontWeight(FontWeight.w700);
+  static get extrabold => GaleFontWeight(FontWeight.w800);
+  static get black => GaleFontWeight(FontWeight.w900);
+}
+
+class GaleFontWeight extends GalePredicate {
+  @override
+  final FontWeight value;
+
+  GaleFontWeight([this.value = FontWeight.normal]) : super(value);
+
+  static get w100 => GaleFontWeight(FontWeight.w100);
+  static get w200 => GaleFontWeight(FontWeight.w200);
+  static get w300 => GaleFontWeight(FontWeight.w300);
+  static get w400 => GaleFontWeight(FontWeight.w400);
+  static get w500 => GaleFontWeight(FontWeight.w500);
+  static get w600 => GaleFontWeight(FontWeight.w600);
+  static get w700 => GaleFontWeight(FontWeight.w700);
+  static get w800 => GaleFontWeight(FontWeight.w800);
+  static get w900 => GaleFontWeight(FontWeight.w900);
+}
+
+mixin hasFontWeight {
+  late List<GalePredicate> predicates;
+
+  FontWeight get fontWeight => predicates.lastWhere((e) => e is GaleFontWeight, orElse: () => GaleFontWeight()).value;
+}
+
+class GaleText extends StatelessWidget with hasTextColor, hasFontWeight implements GaleWidget {
   @override
   late String text;
 
@@ -14,6 +49,6 @@ class GaleText extends StatelessWidget with hasTextColor implements GaleWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: TextStyle(color: textColor));
+    return Text(text, style: TextStyle(color: textColor, fontWeight: fontWeight));
   }
 }
