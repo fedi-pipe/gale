24ada28fa9ba3b3dced85812d7cdd4002e1e3478
malkoG <rijgndqw012@gmail.com>
2024-03-31 22:42:53 +0900

    Add GaleCircle component

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -6,13 +6,36 @@ import 'package:gale/core/widget_base.dart';
 class GaleContainer extends StatelessWidget implements IGaleBgColor {
   late Widget child;
 
+  double? width;
+  double? height;
+
+  late BoxShape shape;
+
   @override
   late List<GalePredicate> predicates;
 
-  GaleContainer({this.child = GW.defaultChild, this.predicates = const [], super.key});
+  GaleContainer(
+      {this.child = GW.defaultChild,
+      this.predicates = const [],
+      this.shape = BoxShape.rectangle,
+      this.width,
+      this.height,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: bgColor, child: child);
+    return Container(
+        child: child,
+        decoration: BoxDecoration(
+          color: bgColor,
+          shape: shape,
+        ),
+        width: width,
+        height: height);
   }
+}
+
+class GaleCircle extends GaleContainer {
+  GaleCircle({Widget child = GW.defaultChild, List<GalePredicate> predicates = const [], required double radius})
+      : super(child: child, predicates: predicates, shape: BoxShape.circle, width: radius, height: radius);
 }
